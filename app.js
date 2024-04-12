018f11a593236def75ee146aca4d395dfe5e392b
andropolis0 <149090629+andropolis0@users.noreply.github.com>
2024-04-12 20:40:18 +0200

    sumthn

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const beats = new Audio("massobeats-honey_jam.mp3");
-let expcoll = 1;
-let testVar = 1;
+let expcoll = 1; //1 = expand, 0 = collapse
 
 function player() {
-    expcoll = !expcoll;
-    if(!expcoll) expandPlayer();
+    if(expcoll) expandPlayer();
     else collapsePlayer();
 }
 
@@ -20,31 +18,43 @@ function updateCurrentTime() {
         console.log(min + ":" + sec);
     } //Ended here// outputs right, but the next issue is line 30 in html
 }
-//collapses too fast only the first time ////////////
+
 function expandPlayer() {
-    if(testVar) { testVar = 0;
-        beats.play();
+    beats.play();
+    let vol = beats.volume*100;
+    let fadein = setInterval( function() {
+        if (vol < 100) { vol += 10; beats.volume = vol / 100; }
+        else { clearInterval(fadein); expcoll = 0;}
+    }, 50);
 
-        let vol = beats.volume*100;
-        let fadein = setInterval( function() {
-            if (vol < 100) { vol += 10; beats.volume = vol / 100; }
-            else { clearInterval(fadein); testVar = 1; }
-        }, 50);
-
-        document.getElementById('musicPlayerDiv').style.width="237px";
-        document.getElementById('musicPlayerDiv').style.height="100px";  
-    }
+    document.getElementById('musicPlayerDiv').style.width="237px";
+    document.getElementById('musicPlayerDiv').style.height="100px";
 }
 function collapsePlayer() {
-    if(testVar) { testVar = 0;
-        let vol = beats.volume*100;
-        let fadeout = setInterval( function() {
-            if (vol > 0) { vol -= 10; beats.volume = vol / 100; }
-            else { clearInterval(fadeout); beats.pause(); testVar = 1;}
-        }, 50);
+    let vol = beats.volume*100;
+    let fadeout = setInterval( function() {
+        if (vol > 0) { vol -= 10; beats.volume = vol / 100; }
+        else { clearInterval(fadeout); beats.pause(); expcoll = 1;}
+    }, 50);
 
-        document.getElementById('musicPlayerDiv').style.width="36px";
-        document.getElementById('musicPlayerDiv').style.height="36px";
-    }
+    document.getElementById('musicPlayerDiv').style.width="36px";
+    document.getElementById('musicPlayerDiv').style.height="36px";
 }
-//make wait time longer for extra measure
+
+
+
+const timeline = document.querySelector('.timeline');
+
+function changeTimelinePosition () {
+    const percentagePosition = (100*audio.currentTime) / audio.duration;
+    timeline.style.backgroundSize = `${percentagePosition}% 100%`;
+    timeline.value = percentagePosition;
+  }
+  
+  audio.ontimeupdate = changeTimelinePosition;
+function changeSeek () {
+    const time = (timeline.value * audio.duration) / 100;
+    audio.currentTime = time;
+}
+
+timeline.addEventListener('change', changeSeek);
