5c1672fab138d9d835c3957b7bc974a8edab55a5
andropolis0 <149090629+andropolis0@users.noreply.github.com>
2024-06-22 17:02:23 +0200

    a

--- script.js
+++ script.js
@@ -1,8 +1,74 @@
-var canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext('2d');
+function loadScript(url, callback) {
+  const script = document.createElement('script');
+  script.src = url;
+  script.type = 'text/javascript';
+  script.onload = callback;
+  document.head.appendChild(script);
+}
+var lenis;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function initializeLenis() {
+  const scrollContainer = document.getElementById("main");
+
+  lenis = new Lenis({
+    wrapper: scrollContainer,
+    content: scrollContainer,
+    duration: 1.2,
+    easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+    direction: "vertical",
+    gestureDirection: "vertical",
+    smooth: true,
+    smoothTouch: false,
+    touchMultiplier: 2,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  
+  requestAnimationFrame(raf);
+}
+loadScript('lenis.js', initializeLenis);
+
+const navTitles = document.querySelectorAll('.nav-title');
+const navLinks = document.querySelectorAll('.nav-link');
+  
+navTitles.forEach(navTitle => {
+  navTitle.addEventListener('click', (event) => {
+    const navTitleStyles = window.getComputedStyle(navTitle, '::after');
+    const right = parseInt(navTitleStyles.right, 10);
+    const width = parseInt(navTitleStyles.width, 10);
+    const top = navTitle.offsetTop;
+    const bottom = top + navTitle.offsetHeight;
+
+    const pseudoBox = {
+      top: top,
+      bottom: bottom,
+      left: navTitle.offsetLeft + navTitle.offsetWidth - right - width,
+      right: navTitle.offsetLeft + navTitle.offsetWidth - right
+    };
+
+    if (event.pageX >= pseudoBox.left && event.pageX <= pseudoBox.right && event.pageY >= pseudoBox.top && event.pageY <= pseudoBox.bottom) {
+      const navTitleText = navTitle.textContent.trim();
+      const contentSegment = Array.from(document.querySelectorAll('.content-segment')).find(element => element.textContent.trim() === navTitleText);
+
+      if (contentSegment) { lenis.scrollTo(contentSegment); }
+    }
+  });
+});
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', () => {
+    const navLinkText = navLink.textContent.trim();
+    const contentTitle = Array.from(document.querySelectorAll('.content-title')).find(element => element.textContent.trim() === navLinkText);
+
+    if (contentTitle) { lenis.scrollTo(contentTitle); }
+  });
+});
+
+var canvas = document.getElementById("canvas"), ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
 var stars = [], FPS = 60, x = ((window.innerWidth * window.innerHeight) / 20000);
 
@@ -50,7 +116,6 @@ function draw() {
   ctx.strokeStyle = gradient;
   ctx.stroke();
 }
-
 function distance( point1, point2 ){
   var xs = 0;
   var ys = 0;
@@ -63,7 +128,6 @@ function distance( point1, point2 ){
  
   return Math.sqrt( xs + ys );
 }
-
 function update() {
   for (var i = 0, x = stars.length; i < x; i++) {
     var s = stars[i];
@@ -75,11 +139,9 @@ function update() {
     if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
   }
 }
-
 function tick() {
   draw();
   update();
   requestAnimationFrame(tick);
 }
-
 tick();
